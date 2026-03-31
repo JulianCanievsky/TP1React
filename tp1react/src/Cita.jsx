2e4b0e1b1bd6c367af82b276f67baff8281c560e
JULIÁN MARIO CANIEVSKY <49550988@ort>
2026-03-31 10:22:13 -0300

    finalizado

---
+++ tp1react/src/Cita.jsx
@@ -0,0 +1,16 @@
+import "./Cita.css";
+
+function Cita({ cita }) {
+  return (
+    <div className="cita">
+      <p>Mascota: <span>{cita.mascota}</span></p>
+      <p>Dueño: <span>{cita.dueño}</span></p>
+      <p>Fecha: <span>{cita.fecha}</span></p>
+      <p>Hora: <span>{cita.hora}</span></p>
+      <p>Sintomas: <span>{cita.sintomas}</span></p>
+      <button className="btn-eliminar">Eliminar ×</button>
+    </div>
+  );
+}
+
+export default Cita;
