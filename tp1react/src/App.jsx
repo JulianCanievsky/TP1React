d0b9821320a91103ab038a01ed442f2dfb5ea2fe
JULIÁN MARIO CANIEVSKY <49550988@ort>
2026-04-21 09:32:39 -0300

    parte3

--- tp1react/src/App.jsx
+++ tp1react/src/App.jsx
@@ -1,27 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Formulario from "./Formulario";
 import Listado from "./Listado";
 import "./App.css";
 
 function App() {
-  const [citas, setCitas] = useState([
-    {
-      id: 1,
-      mascota: "Nina",
-      dueño: "Martin",
-      fecha: "2021-08-05",
-      hora: "08:20",
-      sintomas: "Le duele la pierna",
-    },
-    {
-      id: 2,
-      mascota: "Sifon",
-      dueño: "Flecha",
-      fecha: "2023-08-05",
-      hora: "09:24",
-      sintomas: "Duerme mucho",
-    },
-  ]);
+  const [citas, setCitas] = useState(() => {
+  const datos = localStorage.getItem("citas");
+  return datos ? JSON.parse(datos) : [];
+});
+  useEffect(() => {
+  localStorage.setItem("citas", JSON.stringify(citas));
+}, [citas]);
 
   return (
     <div>
